03bb1f3621bffe61447f204b53849a61d0b63fc5
smileknight <lpwll2023@gmail.com>
2026-09-12 16:54:14 +0800

    day 5

---
+++ leetcode/day_4.py
@@ -0,0 +1,104 @@
+
+'''
+hot problem
+
+给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+
+找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+返回容器可以储存的最大水量。
+
+说明：你不能倾斜容器。
+'''
+
+
+# 有两种方法O(n)2 求解，双指针
+# 双指针方式：
+# left = 0, right = n - 1，从最宽的一对开始往中间收。每轮算一次面积，然后移动矮的那一边。
+# 这里的left和right就是指针
+# while和for循环： while 实际上就相当于是for + if
+height = [1,8,6,2,5,4,8,3,7]
+def max_area(height):
+    left, right = 0 ,len(height) - 1
+    best = 0 
+    while left < right:
+        area = (right - left) * min(height[left], height[right])
+        best = max(best, area)
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+    return best
+
+print(max_area(height))
+
+
+
+
+'''
++-------------+---------+
+| 列名        | 类型     |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
+| salary      | int     |
++-------------+---------+
+employee_id 是这个表的主键(具有唯一值的列)。
+此表的每一行给出了雇员id ，名字和薪水。
+ 
+
+编写解决方案，计算每个雇员的奖金。如果一个雇员的 id 是 奇数 并且他的名字不是以 'M' 开头，那么他的奖金是他工资的 100% ，否则奖金为 0 。
+
+返回的结果按照 employee_id 排序。
+
+返回结果格式如下面的例子所示。
+
+ 
+
+示例 1:
+
+输入：
+Employees 表:
++-------------+---------+--------+
+| employee_id | name    | salary |
++-------------+---------+--------+
+| 2           | Meir    | 3000   |
+| 3           | Michael | 3800   |
+| 7           | Addilyn | 7400   |
+| 8           | Juan    | 6100   |
+| 9           | Kannon  | 7700   |
++-------------+---------+--------+
+输出：
++-------------+-------+
+| employee_id | bonus |
++-------------+-------+
+| 2           | 0     |
+| 3           | 0     |
+| 7           | 7400  |
+| 8           | 0     |
+| 9           | 7700  |
++-------------+-------+
+解释：
+因为雇员id是偶数，所以雇员id 是2和8的两个雇员得到的奖金是0。
+雇员id为3的因为他的名字以'M'开头，所以，奖金是0。
+其他的雇员得到了百分之百的奖金。
+
+lambda表达式使用:
+lambda x: 函数
+'''
+
+
+# 在使用pandas时，能用列运算符，就尽量不要用apply(apply会相对慢很多)
+
+import pandas as pd
+employees = pd.DataFrame({
+    'employee_id': [2, 3, 7, 8, 9],
+    'name': ['Meir', 'Michael', 'Addilyn', 'Juan', 'Kannon'],
+    'salary': [3000, 3800, 7400, 6100, 7700]
+})
+def calculate_bonus(employees):
+    is_odd = employees['employee_id'] % 2 == 1
+    not_m = ~employees['name'].str.startswith('M')
+    employees['bonus'] = employees['salary'].where(is_odd & not_m, 0)
+    return employees[['employee_id', 'bonus']].sort_values('employee_id')
+print(calculate_bonus(employees))
---
+++ leetcode/day_5.py
@@ -0,0 +1,105 @@
+
+'''
+
+hot problem
+
+给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 
+满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+
+示例 1：
+
+输入：nums = [-1,0,1,2,-1,-4]
+输出：[[-1,-1,2],[-1,0,1]]
+解释：
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+注意，输出的顺序和三元组的顺序并不重要。
+
+
+'''
+
+# 首先要构建成有序数组
+# 这个是双指针的进阶，感觉还有点难，可能还要再想想
+nums = [-1,0,1,2,-1,-4]
+def three_sum(nums):
+    nums.sort()
+    n = len(nums)
+    res = []
+    for i in range(n - 2):
+        if nums[i] > 0:
+            break
+        if i > 0 and nums[i] == nums[i - 1]:   # 去重，第一个数不能重复作为起点（确保没有相等的数值）
+            continue
+        left, right = i + 1, n - 1  
+        while left < right:
+            s = nums[i] + nums[left] + nums[right]
+            if s < 0:
+                left += 1
+            elif s > 0:
+                right -= 1
+            else:
+                res.append([nums[i], nums[left], nums[right]])
+                left += 1                       # 这里是进行去重
+                right -= 1
+                while left < right and nums[left] == nums[left - 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right + 1]:
+                    right -= 1
+
+    return res
+
+'''
+pandas
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id 是该表的主键(具有唯一值的列)。
+该表包含用户的 ID 和名字。名字仅由小写和大写字符组成。
+ 
+
+编写解决方案，修复名字，使得只有第一个字符是大写的，其余都是小写的。
+
+返回按 user_id 排序的结果表。
+
+返回结果格式示例如下。
+
+ 
+
+示例 1：
+
+输入：
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+输出：
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
+
+
+'''
+import pandas as pd
+users = pd.DataFrame({
+    'user_id': [1, 2],
+    'name': ['aLice', 'bOB']
+})
+
+def fix_names(users):
+    users['name'] = users['name'].str.capitalize()
+    return users.sort_values('user_id')
+print(fix_names(users))
