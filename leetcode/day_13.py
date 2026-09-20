df03a26348c2fbd1b41077c58135d8ac19df98aa
smileknight <lpwll2023@gmail.com>
2026-09-20 11:20:56 +0800

    day_13

---
+++ leetcode/day_13.py
@@ -0,0 +1,299 @@
+'''
+pandas
+
+
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| actor_id    | int     |
+| director_id | int     |
+| timestamp   | int     |
++-------------+---------+
+timestamp 是这张表的主键(具有唯一值的列).
+ 
+
+编写解决方案找出合作过至少三次的演员和导演的 id 对 (actor_id, director_id)
+
+ 
+
+示例 1：
+
+输入：
+ActorDirector 表：
++-------------+-------------+-------------+
+| actor_id    | director_id | timestamp   |
++-------------+-------------+-------------+
+| 1           | 1           | 0           |
+| 1           | 1           | 1           |
+| 1           | 1           | 2           |
+| 1           | 2           | 3           |
+| 1           | 2           | 4           |
+| 2           | 1           | 5           |
+| 2           | 1           | 6           |
++-------------+-------------+-------------+
+输出：
++-------------+-------------+
+| actor_id    | director_id |
++-------------+-------------+
+| 1           | 1           |
++-------------+-------------+
+
+
+
+
+
+
+Employees 表：
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+在 SQL 中，id 是这张表的主键。
+这张表的每一行分别代表了某公司其中一位员工的名字和 ID 。
+ 
+
+EmployeeUNI 表：
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+在 SQL 中，(id, unique_id) 是这张表的主键。
+这张表的每一行包含了该公司某位员工的 ID 和他的唯一标识码（unique ID）。
+ 
+
+展示每位用户的 唯一标识码（unique ID ）；如果某位员工没有唯一标识码，使用 null 填充即可。
+
+你可以以 任意 顺序返回结果表。
+
+返回结果的格式如下例所示。
+
+ 
+
+示例 1：
+
+输入：
+Employees 表:
++----+----------+
+| id | name     |
++----+----------+
+| 1  | Alice    |
+| 7  | Bob      |
+| 11 | Meir     |
+| 90 | Winston  |
+| 3  | Jonathan |
++----+----------+
+EmployeeUNI 表:
++----+-----------+
+| id | unique_id |
++----+-----------+
+| 3  | 1         |
+| 11 | 2         |
+| 90 | 3         |
++----+-----------+
+输出：
++-----------+----------+
+| unique_id | name     |
++-----------+----------+
+| null      | Alice    |
+| null      | Bob      |
+| 2         | Meir     |
+| 3         | Winston  |
+| 1         | Jonathan |
++-----------+----------+
+解释：
+Alice and Bob 没有唯一标识码, 因此我们使用 null 替代。
+Meir 的唯一标识码是 2 。
+Winston 的唯一标识码是 3 。
+Jonathan 唯一标识码是 1 。
+
+
+
++-------------+---------+
+| 列名         | 类型    |
++-------------+---------+
+| sell_date   | date    |
+| product     | varchar |
++-------------+---------+
+该表没有主键(具有唯一值的列)。它可能包含重复项。
+此表的每一行都包含产品名称和在市场上销售的日期。
+ 
+
+编写解决方案找出每个日期、销售的不同产品的数量及其名称。
+每个日期的销售产品名称应按词典序排列。
+返回按 sell_date 排序的结果表。
+结果表结果格式如下例所示。
+
+ 
+
+示例 1:
+
+输入：
+Activities 表：
++------------+-------------+
+| sell_date  | product     |
++------------+-------------+
+| 2020-05-30 | Headphone   |
+| 2020-06-01 | Pencil      |
+| 2020-06-02 | Mask        |
+| 2020-05-30 | Basketball  |
+| 2020-06-01 | Bible       |
+| 2020-06-02 | Mask        |
+| 2020-05-30 | T-Shirt     |
++------------+-------------+
+输出：
++------------+----------+------------------------------+
+| sell_date  | num_sold | products                     |
++------------+----------+------------------------------+
+| 2020-05-30 | 3        | Basketball,Headphone,T-shirt |
+| 2020-06-01 | 2        | Bible,Pencil                 |
+| 2020-06-02 | 1        | Mask                         |
++------------+----------+------------------------------+
+解释：
+对于2020-05-30，出售的物品是 (Headphone, Basketball, T-shirt)，按词典序排列，并用逗号 ',' 分隔。
+对于2020-06-01，出售的物品是 (Pencil, Bible)，按词典序排列，并用逗号分隔。
+对于2020-06-02，出售的物品是 (Mask)，只需返回该物品名。
+
+
+
+
+'''
+import pandas as pd
+
+
+# pandas 1
+actor_director = pd.DataFrame({
+    'actor_id': [1, 1, 1, 1, 1, 2, 2],
+    'director_id': [1, 1, 1, 2, 2, 1, 1],
+    'timestamp': [0, 1, 2, 3, 4, 5, 6]
+})
+
+
+def actors_and_directors(actor_director):
+    df = actor_director.groupby(['actor_id', 'director_id']).size().reset_index(name='count')
+    # import ipdb; ipdb.set_trace()
+    df = df[df['count'] >= 3][['actor_id', 'director_id']]
+    return df
+print(actors_and_directors(actor_director))
+
+
+# pandas 2
+employees = pd.DataFrame({
+    'id': [1, 7, 11, 90, 3],
+    'name': ['Alice', 'Bob', 'Meir', 'Winston', 'Jonathan']
+})
+employee_uni = pd.DataFrame({
+    'id': [3, 11, 90],
+    'unique_id': [1, 2, 3]
+})
+
+def replace_employee_id(employees, employee_uni):
+    result = pd.merge(employees, employee_uni, on='id', how='left')[['unique_id', 'name']]
+    return result
+print(replace_employee_id(employees, employee_uni))
+
+
+activities = pd.DataFrame({
+    'sell_date': ['2020-05-30', '2020-06-01', '2020-06-02', '2020-05-30', '2020-06-01', '2020-06-02', '2020-05-30'],
+    'product': ['Headphone', 'Pencil', 'Mask', 'Basketball', 'Bible', 'Mask', 'T-Shirt']
+})  
+
+def categorize_products(activities):
+    df = activities.groupby('sell_date').agg(num_sold=('product', 'nunique'), products=('product', lambda x: ','.join(sorted(x.unique())))).reset_index()
+    return df.sort_values('sell_date')
+print(categorize_products(activities))
+
+
+
+'''
+base problem 1
+
+给定一个表示 大整数 的整数数组 digits，其中 digits[i] 是整数的第 i 位数字。这些数字按从左到右，
+从最高位到最低位排列。这个大整数不包含任何前导 0。
+
+将大整数加 1，并返回结果的数字数组。
+
+ 
+
+示例 1：
+
+输入：digits = [1,2,3]
+输出：[1,2,4]
+解释：输入数组表示数字 123。
+加 1 后得到 123 + 1 = 124。
+因此，结果应该是 [1,2,4]。
+
+
+base problem 2
+
+如果数组是单调递增或单调递减的，那么它是 单调 的。
+
+如果对于所有 i <= j，nums[i] <= nums[j]，那么数组 nums 是单调递增的。 如果对于所有 i <= j，nums[i] >= nums[j]，那么数组 nums 是单调递减的。
+
+当给定的数组 nums 是单调数组时返回 true，否则返回 false。
+
+ 
+
+示例 1：
+
+输入：nums = [1,2,2,3]
+输出：true
+示例 2：
+
+输入：nums = [6,5,4,4]
+输出：true
+
+
+base problem 3
+
+给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中 最后一个 单词的长度。
+
+单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+
+ 
+
+示例 1：
+
+输入：s = "Hello World"
+输出：5
+解释：最后一个单词是“World”，长度为 5。
+
+
+
+'''
+
+
+digits = [1, 2, 3]
+def plusOne(digits):
+    for i in range(len(digits) - 1, -1, -1):            # 关键在于从后向前做加法进位次
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        digits[i] = 0
+    return [1] + digits
+print(plusOne(digits))
+
+
+
+nums = [1, 2, 2, 3]
+def isMonotonic(nums):                      # 这道题不采用判断方向再判断是否等差的方式，因为方向不是很好判断，现在是依次移动两格的方式来判断差值
+    increasing = decreasing = True                  
+    for i in range(1, len(nums)):                   # 进行相邻配对
+        if nums[i] > nums[i - 1]:
+            decreasing = False
+        if nums[i] < nums[i - 1]:
+            increasing = False
+    return increasing or decreasing
+print(isMonotonic(nums))
+
+
+s = "Hello World"
+def lengthOfLastWord(s):
+    words = s.strip().split()                 # 先去掉首尾空格，再按空格分割
+    return len(words[-1])                     # 返回最后一个单词的长度
